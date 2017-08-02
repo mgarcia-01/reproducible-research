53f0e1ec547df9c2f2d5c356b50a9e2736ccd787
Michael Garcia <mgarcia4703@gmail.com>
2017-08-02 03:18:56 -0700

    new files

--- download files.R
+++ download files.R
@@ -1,7 +1,12 @@
 ### Week 1 Project ###
 library(data.table)
+
 webURL <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2Factivity.zip"
-destURL <- file.path(getwd(),paste("activity_monitoring_data",".csv",sep = ""))
-actMonitor <- read.csv(destURL)
-                 
-download.file(webURL,method = "auto", destfile = destURL)
+destURL <- file.path(getwd(),paste("activity_monitoring_data",".zip",sep = ""))
+localFileURL <- file.path(getwd(),paste("activity",".csv",sep = ""))
+download.file(webURL,destfile = destURL)
+
+unzip(destURL)
+activityData <- as.data.frame(fread(localFileURL))
+activityData$steps <- as.numeric(activityData$steps)
+activityData$interval <- as.numeric(activityData$interval)
---
+++ histogram_steps_taken.R
@@ -0,0 +1,60 @@
+#   2) Histogram of the total number of steps taken each day
+install.packages("astsa", dependencies = TRUE)
+# if we do not remove na's then the total is skewed towards 0 
+activityDataAg <- aggregate(activityData$steps,list(activityData$date),sum)
+names(activityDataAg) <- c("date","totalSteps")
+#activityDataAg <- activityDataAg[complete.cases(activityDataAg),]
+hist(activityDataAg$totalSteps, xlab = "TotalSteps", breaks = 10)
+
+##3) Mean and median number of steps taken each day
+
+
+stepSummary <- as.table(summary(activityDataAg$totalSteps))
+stepsMedian <- stepSummary[3]
+stepsMean <- stepSummary[4]
+
+
+# 4) Time series plot of the average number of steps taken
+# removed na's
+actDataAg <- activityDataAg[complete.cases(activityDataAg),]
+#activityTS= ts(rnorm(48), start=c(2293,6), frequency=1)
+plot(actDataAg$date,actDataAg$totalSteps,type = "l")
+text(dow_icefrozn$order_dow, (as.numeric(dow_icefrozn$no_dist_orders)/1000)
+     ,labels = round((as.numeric(dow_icefrozn$no_dist_orders)/1000))
+     , cex=0.85, font = 4, pos= 3, col="black")
+
+
+
+
+axis.Date(1, at = seq(actDataAg[1], actDataAg[100], length.out=25),
+          labels = seq(actDataAg[1], actDataAg[100], length.out=25),
+          format= "%m/%d/%Y", las = 2)
+time(actDataAg)
+
+
+
+actDataAg$date <- as.Date(actDataAg$date,format = "%y-%m-%d")
+plot(actDataAg$totalSteps~as.Date(actDataAg$date,"%y-%m-%d"),type="l",
+     xlab="Date",ylab="Total Steps")
+
+
+activityTSdd<-data.frame(activityDate=seq(as.Date("1999-01-01"), as.Date("2014-01-10"), by="6 mon"),
+  salpr = cumsum(rnorm(31))
+)
+
+
+
+ggplot(data=actDataAg, aes(x=actDataAg$date, y=actDataAg$totalSteps)) +
+  geom_bar(stat="sum") + theme(axis.text.x = element_text(angle = 90, hjust = 1)
+                               ,axis.title.x = element_text(face="bold", size=12)
+                               ,legend.position="none"
+  )+labs(x="Date",y="Total Steps")
+
+geom_abline(data=actDataAg, aes(x=actDataAg$date, y=actDataAg$totalSteps))+ggplot()
+
+
+ggplot(data=actDataAg, aes(x=actDataAg$date, y=actDataAg$totalSteps)) + geom_point()
+  geom_line()+theme(axis.text.x = element_text(angle = 90, hjust = 1)
+                    ,axis.title.x = element_text(face="bold", size=12)
+                    ,legend.position="none"
+  )+labs(x="Date",y="Total Steps")
